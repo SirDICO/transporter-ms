e2fb9756673e5f5177579977162e78887ab212d1
Sir DICO <88980314+SirDICO@users.noreply.github.com>
2022-07-01 10:44:20 +0100

    first-sb-commit

---
+++ src/test/java/com/transporter/transporterms/TransporterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transporter.transporterms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransporterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
